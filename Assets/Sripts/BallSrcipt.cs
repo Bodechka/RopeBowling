93b013972cf1dc5e499410556b64727b5d0786c5
Bodechka <fuckyou@gmail.com>
2020-02-15 21:03:45 +0200

    Init

---
+++ Assets/Sripts/BallSrcipt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSrcipt : MonoBehaviour
+{
+    
+    public GameObject sp;
+    private SpriteRenderer spriteR;
+    private Sprite[] sprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        spriteR.sprite = Resources.Load<Sprite>("football");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
